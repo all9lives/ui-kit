7b9f2b88a93b989715dfcddd01ec85a931740cec
JerryShen <jerry82422@gmail.com>
2017-07-28 17:27:03 +0800

    Fix android connect error

--- storybook/native/index.android.js
+++ storybook/native/index.android.js
@@ -1,3 +1,13 @@
-import StorybookUI from './storybook';
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions, global-require */
 
+import { AppRegistry } from 'react-native';
+import { getStorybookUI, configure } from '@storybook/react-native';
+
+// import stories
+configure(() => {
+  require('../../stories/preface');
+}, module);
+
+const StorybookUI = getStorybookUI({ port: 7007, host: '10.0.0.1' });
+AppRegistry.registerComponent('core_ui_kit', () => StorybookUI);
 export default StorybookUI;
