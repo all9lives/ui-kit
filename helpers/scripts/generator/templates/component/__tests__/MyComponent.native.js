519469ba71710bd449bcb77b123d0268d91b3a21
JerryShen <jerry82422@gmail.com>
2017-07-30 11:21:06 +0800

    Update testing setting
    + Add `jest-styled-components` for handling classNames
    + Change `jest-serializer-enzyme` to `enzyme-to-json`

--- helpers/scripts/generator/templates/component/__tests__/MyComponent.native.js
+++ helpers/scripts/generator/templates/component/__tests__/MyComponent.native.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import MyComponent from '../index.ios.js'
 
-it('render snapshot for `MyComponent` on `native`', () => {
+it('renders a snapshot for `MyComponent` on `native`', () => {
   const wrapper = shallow(
     <MyComponent>Apple</MyComponent>
   )
--- helpers/scripts/generator/templates/component/__tests__/MyComponent.web.js
+++ helpers/scripts/generator/templates/component/__tests__/MyComponent.web.js
@@ -1,9 +1,10 @@
 /* eslint-env jest */
+import 'jest-styled-components'
 import React from 'react'
 import { shallow } from 'enzyme'
 import MyComponent from '../index.js'
 
-it('render snapshot for `MyComponent` on `web`', () => {
+it('renders a snapshot for `MyComponent` on `web`', () => {
   const wrapper = shallow(
     <MyComponent>Apple</MyComponent>
   )
