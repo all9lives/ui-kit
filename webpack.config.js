4af03aa46ed1fc7df317cb38f3ea9389e6489155
JerryShen <jerry82422@gmail.com>
2017-08-02 22:46:29 +0800

    Add `webpack` for build

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+
+  target: 'node',
+
+  entry: './src/index.js',
+
+  output: {
+    path: path.join(__dirname, 'lib'),
+    filename: 'main.js',
+    libraryTarget: 'commonjs'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [{
+          loader: 'babel-loader'
+        }]
+      }
+    ]
+  },
+
+  externals: [nodeExternals()]
+
+}
