69cd874f7e5af5be3a2ac782ed235db75ab7dcf3
JerryShen <jerry82422@gmail.com>
2017-08-12 10:01:17 +0800

    Add more test cases for `FloatLabelInput.native`

--- src/FloatLabelInput/__tests__/FloatLabelInput.native.js
+++ src/FloatLabelInput/__tests__/FloatLabelInput.native.js
@@ -1,7 +1,22 @@
 /* eslint-env jest */
 import React from 'react'
 import { shallow } from 'enzyme'
-import FloatLabelInput from '../index.ios.js'
+import FloatLabelInput, { AnimatedInput, AnimatedLabel } from '../index.ios.js'
+
+jest.mock('Animated', () => {
+  const ActualAnimated = require.requireActual('Animated')
+  return {
+    ...ActualAnimated,
+    timing: (value, config) => {
+      return {
+        start: (callback) => {
+          value.setValue(config.toValue)
+          callback && callback()
+        }
+      }
+    }
+  }
+})
 
 describe('FloatLabelInput.native', () => {
   it('renders a snapshot', () => {
@@ -13,6 +28,15 @@ describe('FloatLabelInput.native', () => {
 
   it('renders a snapshot with `handleFocus`', () => {
     const wrapper = shallow(
+      <FloatLabelInput onFocus={() => 'onFocused'} />
+    )
+    wrapper.instance().handleFocus()
+    wrapper.update()
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders a snapshot without `onFocus`', () => {
+    const wrapper = shallow(
       <FloatLabelInput />
     )
     wrapper.instance().handleFocus()
@@ -22,11 +46,97 @@ describe('FloatLabelInput.native', () => {
 
   it('renders a snapshot with `handleBlur`', () => {
     const wrapper = shallow(
+      <FloatLabelInput onBlur={() => 'onBlurred'} />
+    )
+    wrapper.instance().handleBlur()
+    wrapper.update()
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders a snapshot without `onBlur`', () => {
+    const wrapper = shallow(
       <FloatLabelInput />
     )
-    wrapper.instance().handleFocus()
-    wrapper.update()
     wrapper.instance().handleBlur()
+    wrapper.update()
+    expect(wrapper).toMatchSnapshot()
+  })
+})
+
+describe('FloatLabelInput.AnimatedLabel.native', () => {
+  it('renders a snapshot', () => {
+    const wrapper = shallow(
+      <AnimatedLabel />
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders a snapshot with `show`', () => {
+    const wrapper = shallow(
+      <AnimatedLabel />
+      , { lifecycleExperimental: true })
+    wrapper.setProps({ show: true })
+    wrapper.update()
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders a snapshot with `show` changing to `show:false`', () => {
+    const wrapper = shallow(
+      <AnimatedLabel show />
+      , { lifecycleExperimental: true })
+    wrapper.setProps({ show: false })
+    wrapper.update()
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders a snapshot with `show` unchanged', () => {
+    const wrapper = shallow(
+      <AnimatedLabel show />
+      , { lifecycleExperimental: true })
+    wrapper.setProps({ show: true })
+    wrapper.update()
+    expect(wrapper).toMatchSnapshot()
+  })
+})
+
+describe('FloatLabelInput.AnimatedInput.native', () => {
+  it('renders a snapshot', () => {
+    const wrapper = shallow(
+      <AnimatedInput />
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders a snapshot with `value:undefined` changing to `value:bar`', () => {
+    const wrapper = shallow(
+      <AnimatedInput />
+    )
+    wrapper.setProps({ value: 'foo' })
+    wrapper.update()
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders a snapshot with `value:foo`', () => {
+    const wrapper = shallow(
+      <AnimatedInput value='foo' />
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders a snapshot with `value:foo` changing to `value:empty`', () => {
+    const wrapper = shallow(
+      <AnimatedInput value='foo' />
+    )
+    wrapper.setProps({ value: '' })
+    wrapper.update()
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders a snapshot with `value:foo` unchanged', () => {
+    const wrapper = shallow(
+      <AnimatedInput value='foo' />
+    )
+    wrapper.setProps({ value: 'foo' })
     wrapper.update()
     expect(wrapper).toMatchSnapshot()
   })
