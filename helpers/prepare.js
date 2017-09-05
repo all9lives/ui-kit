4686feef1c3c5e90f3de0f63a713725c385ab440
JerryShen <jerry82422@gmail.com>
2017-09-05 09:40:52 +0800

    Remove `helpers/prepare`

--- helpers/prepare.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-const obj = JSON.parse(fs.readFileSync('package.json', 'utf8'))
-
-obj.main = './lib/main.js'
-
-fs.writeFileSync('package.json', JSON.stringify(obj, null, 2), 'utf8')
