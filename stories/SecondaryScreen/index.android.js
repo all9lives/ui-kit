62dd1b4cebcabed3a33d931260d66f629957eee5
JerryShen <jerry82422@gmail.com>
2017-10-13 12:06:23 +0800

    Separate `SecondaryScreen` stories

---
+++ stories/SecondaryScreen/index.android.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import createStory from '../../storybook/createStory'
+import ScreenStuff from '../../storybook/createStory/ScreenStuff'
+import SecondaryScreen from '../../src/SecondaryScreen'
+import Text from '../../src/Text'
+
+export const main = () =>
+createStory('SecondaryScreen', module)
+  .add('default', () => (
+    <SecondaryScreen>
+      <ScreenStuff />
+    </SecondaryScreen>
+  ))
+  .add('with title `MyTitle`', () => (
+    <SecondaryScreen title='MyTitle'>
+      <ScreenStuff />
+    </SecondaryScreen>
+  ))
+  .add('with actionButtons', () => (
+    <SecondaryScreen actionButtons={[
+      <Text>Button 1</Text>,
+      <Text>Button 2</Text>
+    ]}>
+      <ScreenStuff />
+    </SecondaryScreen>
+  ))
+  .add('with whiteBackIcon', () => (
+    <SecondaryScreen whiteBackIcon>
+      <ScreenStuff />
+    </SecondaryScreen>
+  ))
+
+main()
--- stories/SecondaryScreen/index.js
+++ stories/SecondaryScreen/index.js
@@ -1,30 +1,10 @@
 import React from 'react'
 import createStory from '../../storybook/createStory'
-import ScreenStuff from '../../storybook/createStory/ScreenStuff'
-import SecondaryScreen from '../../src/SecondaryScreen'
 import Text from '../../src/Text'
 
 createStory('SecondaryScreen', module)
   .add('default', () => (
-    <SecondaryScreen>
-      <ScreenStuff />
-    </SecondaryScreen>
-  ))
-  .add('with title `MyTitle`', () => (
-    <SecondaryScreen title='MyTitle'>
-      <ScreenStuff />
-    </SecondaryScreen>
-  ))
-  .add('with actionButtons', () => (
-    <SecondaryScreen actionButtons={[
-      <Text>Button 1</Text>,
-      <Text>Button 2</Text>
-    ]}>
-      <ScreenStuff />
-    </SecondaryScreen>
-  ))
-  .add('with whiteBackIcon', () => (
-    <SecondaryScreen whiteBackIcon>
-      <ScreenStuff />
-    </SecondaryScreen>
+    <Text>
+      It only supports native platform.
+    </Text>
   ))
