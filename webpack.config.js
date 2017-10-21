ff3d6837123101b6cf310b98b218a2c332857ae1
JerryShen <jerry82422@gmail.com>
2017-10-21 20:13:49 +0800

    Add `file-loader` for image require

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,15 @@ module.exports = {
         use: [{
           loader: 'babel-loader'
         }]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
       }
     ]
   },
