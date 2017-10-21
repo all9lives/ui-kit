9fe1c03ad806dea951585eec2222d9b0cddd9d77
JerryShen <jerry82422@gmail.com>
2017-10-21 20:01:38 +0800

    Update `Size`

--- src/utils/Size/index.android.js
+++ src/utils/Size/index.android.js
@@ -1,14 +1,10 @@
 import { Platform, Dimensions } from 'react-native'
+import base from './base'
 
 const SCREEN_HEIGHT_ADJUST = Platform.OS === 'ios' ? 0 : 25
 
 export default {
   screenWidth: Dimensions.get('window').width,
   screenHeight: Dimensions.get('window').height - SCREEN_HEIGHT_ADJUST, // status bar
-  icon: 24,
-  spacing: 10,
-  small: 14,
-  regular: 16,
-  large: 18,
-  title: 25
+  ...base
 }
--- src/utils/Size/index.js
+++ src/utils/Size/index.js
@@ -1,10 +1,7 @@
+import base from './base'
+
 export default {
   windowHeight: '100vh',
   windowWidth: '100vw',
-  icon: 24,
-  spacing: 10,
-  small: 14,
-  regular: 16,
-  large: 18,
-  title: 25
+  ...base
 }
