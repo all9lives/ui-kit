76fde8993b645d884e915effa40f5a149fb14da0
JerryShen <jerry82422@gmail.com>
2017-10-30 18:04:52 +0800

    Fix `Button` margin format

--- src/Button/index.js
+++ src/Button/index.js
@@ -1,14 +1,16 @@
 import styled from 'styled-components'
 import { invert } from '../utils/Color'
-import base, { attrs } from './base'
+import { attrs } from './base'
 import defaultProps from './defaultProps'
+import Unit from '../utils/Unit'
+import Size from '../utils/Size'
 
 const Button = styled.button.attrs({
   ...attrs,
   children: props => (props.children || props.title),
   onClick: props => (props.onClick || props.onPress)
 })`
-  ${base}
+  margin: ${props => Unit.parse(props.margin, (margin) => margin * Size.spacing)};
   background: none;
   border: 0;
   font: inherit;
@@ -20,6 +22,7 @@ const Button = styled.button.attrs({
   color: ${props => invert(props.color, true)};
   background-color: ${props => props.color};
   opacity: ${props => props.disabled ? '0.4' : 1};
+  font-size: ${props => Unit.convert(Size.small)};
 
   :active {
     transform: translate3d(1px, 1px, 0);
