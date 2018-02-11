3ba6ea54d1a866ededa1dfebd18c8d0af2f81fc5
JerryShen <jerry82422@gmail.com>
2018-02-12 07:49:27 +0800

    Add `react-native-fast-image` as peerDependencies

--- src/Image/index.android.js
+++ src/Image/index.android.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components/native'
-import { Image as NativeImage } from 'react-native'
+import NativeImage from 'react-native-fast-image'
 import base, { attrs, filtered } from './base'
 import defaultProps from './defaultProps'
 import { filterProps, mapProps } from '../utils/Misc'
@@ -11,16 +11,16 @@ Base.displayName = 'Image'
 
 export const Styled = styled(Base).attrs(attrs)`
   ${base}
-  resizeMode: ${props => props.fit};
 `
 Styled.defaultProps = {
   ...defaultProps,
   src: ''
 }
 
-const Image = mapProps(({ src, source, ...rest }) => ({
+const Image = mapProps(({ src, source, fit, ...rest }) => ({
   ...rest,
-  source: getRealSource(src || source)
+  source: getRealSource(src || source),
+  resizeMode: fit
 }))(Styled)
 
 export default Image
